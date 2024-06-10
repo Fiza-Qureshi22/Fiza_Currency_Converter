c247fb08cd3d3e5a4c611066e0e102381fb5609e
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-10 23:01:09 +0500

    no changes
    "Implementing currency converter functionality for internationalization

---
+++ main.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node 
+
+import inquirer from "inquirer";
+
+const Currency: any = {
+    USD: 1, //Base Currency
+    EUR: 0.91,
+    GPB: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+
+ let user_answer  = await inquirer.prompt ([
+    {
+    name: "from",
+    message: "Enter From Currency?",
+    type: "list",
+    choices: ["USD", "EUR","GBP", "INR","PKR",]
+   },
+   {
+    name: "to",
+    message: "Enter To Currency?",
+    type: "list",
+    choices: ["USD", "EUR","GBP", "INR","PKR",]
+   },
+   {
+    name: "amount",
+    message: "Enter Your Amount",
+    type: "number",
+   }
+   
+] 
+
+); 
+let fromAmount = Currency[user_answer.from]
+let toAmount = Currency[user_answer.to]
+let amount = user_answer.amount
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount)
+ 
+
